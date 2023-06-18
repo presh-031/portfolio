a2a3917ed0fe75030a8165ff7d38be6050e940b8
presh-031 <akingbuluge.dev@gmail.com>
2023-06-18 00:39:30 -0700

    style: finished structuring and styling project page

--- app/data.ts
+++ app/data.ts
@@ -2,8 +2,17 @@ export const projects = [
   {
     id: "audiophile",
     about:
-      " Audiophile is a fully-fledged e-commerce platform meticulously designed for audiophiles seeking a seamless shopping experience for top-notch headphones, speakers, and earphones. The website offers a diverse range of high-quality audio products, ensuring that discerning users can find their perfect audio companion effortlessly. With Audiophile, users can explore an extensive catalog of products, accompanied by detailed product descriptions that provide in-depth insights into each item's features and specifications. The website also showcases comprehensive product images, allowing users to visualize the products before making a purchase.  I understand the importance of presentation, which is why Audiophile provides a unique feature that displays the contents of each product box. This allows users to get a comprehensive understanding of the accessories and components that come with their chosen audio equipment. To enhance the shopping experience, Audiophile offers a seamless cart management system. Users can easily add products to their cart, review the items selected, and proceed to the secure checkout process. I'm also excited to announce that I will soon integrate Stripe, a trusted payment gateway, ensuring a smooth and secure payment experience. ",
-    techStack:
-      "react, next.js, react-redux, redux-persist, react-popup, tailwind",
+      "Audiophile is a full-fledged e-commerce website built specifically for audiophiles to shop for high-quality headphones, speakers and earphones with ease. Users can browse products, read product descriptions, see items in each product box, see product images, and browse similar products. Users can also manage their cart and checkout wit ease (stripe integration coming soon!)",
+    techStack: [
+      "react",
+      "next.js",
+      "tailwind",
+      "react-popup",
+      "react-redux",
+      "redux-persist",
+      "react-hot-toast",
+    ],
+    github: "https://github.com/presh-031/audiophile",
+    live: "https://audiophile-navy-mu.vercel.app/",
   },
 ];
--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -3,7 +3,10 @@ import React from "react";
 
 import { useRouter } from "next/navigation";
 import { projects } from "@/app/data";
+import Image from "next/image";
 
+import react from "../../../assets/react.png";
+import { BiCodeAlt } from "react-icons/bi";
 const Project = ({ params }: { params: { id: string } }) => {
   const router = useRouter();
 
@@ -13,19 +16,60 @@ const Project = ({ params }: { params: { id: string } }) => {
 
   if (project.length === 0) {
     router.push("/");
+    return;
   }
   return (
     // should params be passed as a prop in next 13 ??
 
-    <div className="min-h-screen bg-black px-6 uppercase text-white">
+    <div className="min-h-screen bg-black px-6  text-white">
       <button
         type="button"
         onClick={() => router.back()}
-        className=" mt-16 text-3xl underline"
+        className=" mt-16 p-2 text-3xl underline"
       >
         Back
       </button>
-      <h1 className="mt-12 text-center text-6xl">Audiophile</h1>
+      <h1 className="mt-12 text-center text-6xl uppercase">{project[0].id}</h1>
+
+      <p className="mt-12 text-3xl uppercase text-[#ebebeb]">About</p>
+      <p className=" mt-6 text-2xl text-[#d6d6d6]">{project[0].about}</p>
+
+      <p className="mt-12 text-3xl uppercase text-[#ebebeb]">Built with</p>
+      <div className="mt-6 grid grid-cols-3 gap-8 text-xl text-[#d6d6d6]">
+        {project[0].techStack.map((tech) => {
+          return (
+            <div
+              key={tech}
+              className="flex h-[8rem] flex-col items-center justify-between"
+            >
+              <Image src={react} alt={tech} width={50} height={50} />
+              <p className="capitalize">{tech}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <p className="mt-12 text-3xl uppercase text-[#ebebeb]">Links</p>
+      <div className="mt-8 flex gap-8">
+        <a
+          className="flex w-[10rem] items-center justify-center rounded bg-gray-200 py-6   text-3xl text-gray-700"
+          href={project[0].github}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Code <BiCodeAlt />
+        </a>
+        <a
+          className="inline-block w-[10rem] rounded bg-[#634eec] py-6 text-center  text-3xl text-white"
+          href={project[0].live}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Live
+        </a>
+      </div>
+
+      <p className="mt-12 text-3xl uppercase text-[#ebebeb]">Screeshots</p>
     </div>
   );
 };
