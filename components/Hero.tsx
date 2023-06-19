8e0a953fa13254f04604e221ac9450fbb12dd877
presh-031 <akingbuluge.dev@gmail.com>
2023-06-18 17:27:38 -0700

    feat: added self img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import { league_gothic } from "@/app/fonts";
+import self from "../assets/self.jpg";
 const Hero = () => {
   return (
-    <div className="bg-black px-6">
+    <div className="bg-black px-6 pt-[8rem]">
+      <Image
+        className="mx-auto h-[20rem] w-[20rem] rounded-full object-cover"
+        src={self}
+        alt="me"
+        width={200}
+        height={200}
+      />
       <h1
-        className={`${league_gothic.className} pt-8 text-center text-[10rem] font-normal leading-[20rem] text-white`}
+        className={`${league_gothic.className}  text-center text-[10rem] font-normal  text-white`}
       >
         PRESH
       </h1>
 
       <section className="text-center text-white">
-        <p className="text-[1.8rem] font-medium">Frontend wizardry.</p>
+        <p className="text-[1.8rem] font-medium">Software Engineer.</p>
         <div className="hidden">
           <p>Github</p>
           <p>Resume</p>
