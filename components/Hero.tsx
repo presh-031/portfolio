35857042a66a0fef95a5b8ceb62b4183aad8ee9d
presh-031 <akingbuluge.dev@gmail.com>
2023-07-13 22:03:57 -0700

    feat: testing styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import Image from "next/image";
 import { league_gothic } from "@/app/fonts";
 import self from "../assets/self.jpg";
-import { BsArrowDown, BsArrowDownUp } from "react-icons/bs";
+import { BsArrowDown, BsArrowDownUp, BsGithub } from "react-icons/bs";
 import ScrollDownMouse from "./ScrollDownMouse";
+import { AiOutlineTwitter, AiFillLinkedin } from "react-icons/ai";
+// import { BiLogoGithub } from "react-icons/bi";
+
+import "../app/animations.css";
+
 const Hero = () => {
   return (
     <div className=" pt-[8rem]">
@@ -13,9 +18,12 @@ const Hero = () => {
         alt="me"
         placeholder="blur"
       />
-
+      <div className="mx-auto mt-6 flex w-fit items-center gap-4 rounded-md border-[1px] px-4 py-2 text-white outline">
+        <div className="animate-pulse-light  h-[.8rem] w-[.8rem] rounded-full bg-green-500"></div>
+        <p>HIRE ME</p>
+      </div>
       <h1
-        className={`text-center text-[10rem] font-bold text-white  max-[350px]:text-[8rem] max-[300px]:text-[6rem] md:text-[20rem] md:leading-[30rem]  lg:text-[29rem] `}
+        className={`text-center text-[4rem] font-bold text-white  max-[350px]:text-[8rem] max-[300px]:text-[6rem] md:text-[20rem] md:leading-[30rem]  lg:text-[29rem] `}
       >
         PRESH
       </h1>
@@ -31,6 +39,16 @@ const Hero = () => {
         <p className="text-[1.2rem] font-medium md:text-[2.6rem]">
           Frontend Web Developer
         </p>{" "}
+        <div className="mt-4 flex items-center justify-center gap-4 text-4xl">
+          <a href="https://github.com/presh-031" target="_blank">
+            <BsGithub />
+          </a>
+          <a href="https://twitter.com/presh-031" target="_blank">
+            <AiOutlineTwitter />
+          </a>
+
+          <AiFillLinkedin />
+        </div>
       </section>
 
       <div className="hidden  text-white   md:flex md:justify-between md:text-4xl">
